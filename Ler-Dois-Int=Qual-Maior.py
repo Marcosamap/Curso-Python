28310d328a9a08eada53c5bcc5968e954ceede4e
MARCOSSQ <marcosamap1978237@gmail.com>
2022-02-19 01:45:23 -0300

    Curso_Python

---
+++ Ler-Dois-Int=Qual-Maior.py
@@ -0,0 +1,6 @@
+num1 = int(input(' Digite um número '))
+num2 = int(input(' Digite outro número '))
+if num1 > num2:
+    print(' Maior número ', num1)
+if num2 > num1:
+    print('Maior número ', num2)

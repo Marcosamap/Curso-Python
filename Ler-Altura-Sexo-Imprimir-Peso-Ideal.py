515cfa79bc0b8886607b745357912eea95ad51d5
MarcosSQ <marcosamap19782327@gmail.com>
2022-02-22 15:41:50 -0300

    Continuando

--- Ler-Altura-Sexo-Imprimir-Peso-Ideal.py
+++ Ler-Altura-Sexo-Imprimir-Peso-Ideal.py
@@ -1,3 +1,8 @@
-altura = int(input(' Digite sua altura '))
-sexo = str(input(' Sexo (M) ou (F)', ))
-print(sexo)
+altura = float(input(' Digite sua altura '))
+sexo = input('Digite seu sexo (M) ou (F) ')
+peso_ideal_h = (72.7 * altura) - 58
+peso_ideal_m = (62.1 * altura) - 44.7
+if sexo == "M":
+    print(' Sexo masculino ', peso_ideal_h)
+else:
+    print(' Feminino ', peso_ideal_m)
